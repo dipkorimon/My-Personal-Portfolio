32d33430b7bba925b923a08030e562d482095d28
dipkorimon <dipkorimon@gamil.com>
2023-07-20 18:15:10 +0600

    added skills

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Intro from "../../components/intro/Intro";
+import Skill from "../../components/skill/Skill";
 
 const Home = () => {
   return (
     <div>
       <Intro />
+      <Skill />
     </div>
   );
 };
